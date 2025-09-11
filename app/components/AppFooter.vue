5e2d209389fa4bead9d14d2354608264c4cd669f
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2025-09-11 19:42:08 +0500

    fix footer

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,5 +1,21 @@
 <template>
   <footer class="relative dark:bg-brand-element-dark mt-auto rounded-t-2xl py-9 overflow-hidden">
+    <span class="dark:hidden absolute -top-0 left-1/2 -translate-x-1/2">
+      <svg xmlns="http://www.w3.org/2000/svg" width="1920" height="226" viewBox="0 0 1920 226" fill="none">
+        <g filter="url(#filter0_f_1688_24868)">
+          <path d="M1459.97 -174.504C1459.97 90.8492 1371.85 -174.504 962.409 -174.504C552.971 -174.504 235.044 58.8182 235.044 -206.535C235.044 -471.889 566.959 -687 976.396 -687C1385.83 -687 1459.97 -439.858 1459.97 -174.504Z" fill="#CF52FE"/>
+        </g>
+        <defs>
+          <filter id="filter0_f_1688_24868" x="-418.252" y="-1340.3" width="2531.52" height="1937.02" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+            <feGaussianBlur stdDeviation="326.648" result="effect1_foregroundBlur_1688_24868"/>
+          </filter>
+        </defs>
+      </svg>
+    </span>
+    <span class="dark:hidden absolute left-1/2 -translate-x-20 -top-1/2 bg-[#6841FE] w-[1505px] h-[973px] rounded-[1505px] blur-[326.6479797363281px]"></span>
+    <span class="dark:hidden absolute right-1/2 -translate-x-20 -top-1/2 bg-[#11E5B3] w-[2302.868px] h-[1143.268px] rounded-[2302.868px] blur-[326.6479797363281px]"></span>
     <span class="absolute -bottom-10 left-1/2 -translate-x-1/2 z-0 bg-transparent">
       <svg xmlns="http://www.w3.org/2000/svg" width="595" height="226" viewBox="0 0 595 226" fill="none">
         <g opacity="0.8" filter="url(#filter0_f_1688_8292)">
